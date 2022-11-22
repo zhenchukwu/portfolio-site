6cfe4fcc53f1efa5898f62df58350ce6641b6c4c
zhenchukwu <zhenchukwu@gmail.com>
2022-11-22 18:21:15 +0100

    new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.jsx 	new file:   src/components/about/About.jsx 	new file:   src/components/about/about.css 	new file:   src/components/contact/Contact.jsx 	new file:   src/components/contact/contact.css 	new file:   src/components/intro/Intro.jsx 	new file:   src/components/intro/intro.css 	new file:   src/components/product/Product.jsx 	new file:   src/components/product/product.css 	new file:   src/components/productList/ProductList.jsx 	new file:   src/components/productList/productList.css 	new file:   src/components/toggle/Toggle.jsx 	new file:   src/components/toggle/toggle.css 	new file:   src/context.js 	new file:   src/data.js 	new file:   src/img/address.png 	new file:   src/img/award.png 	new file:   src/img/email.png 	new file:   src/img/me.jpg 	new file:   src/img/moon.png 	new file:   src/img/phone.png 	new file:   src/img/sun.png 	new file:   src/index.js 	new file:   yarn.lock

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://static.wixstatic.com/media/311dce_09dd46729e0e4ef1a1459e6ea6ed0476~mv2.png/v1/fill/w_674,h_778,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/311dce_09dd46729e0e4ef1a1459e6ea6ed0476~mv2.png",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+    {
+      id: 2,
+      img: "https://static.wixstatic.com/media/0e0314_6932dff3b9944958aabe77f06341f269~mv2.jpg",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+    {
+      id: 3,
+      img: "https://static.wixstatic.com/media/311dce_e79c5924d90d4e82992b123dbf72233b~mv2.png/v1/fill/w_835,h_370,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1_2_webp.png",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+    {
+      id: 4,
+      img: "https://static.wixstatic.com/media/0e0314_070ce5a94ce440aebed8e3c363dc3c3c~mv2.jpg",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+    {
+      id: 5,
+      img: "https://static.wixstatic.com/media/94a51f_f1ae57c171e5491b90cea6f1709727fa~mv2.jpg",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+    {
+      id: 6,
+      img: "https://static.wixstatic.com/media/0e0314_85c768cc81df4bde96eedcdc1e88a940~mv2.jpg",
+      link: "https://www.linkedin.com/in/christopher-chukwu-229b81169",
+    },
+  ];
